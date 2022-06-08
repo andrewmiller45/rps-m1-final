17eb8d20c4defb40ed68d7d6929ae9bb201cf50f
Andrew Miller <andrew.r.miller45@gmail.com>
2022-06-08 10:34:23 -0700

    modify params to game class

--- game.js
+++ game.js
@@ -1,9 +1,9 @@
 class Game {
-    constructor(playerOne, playerTwo, gameMode){
+    constructor(){
         this.id = Date.now()
         this.playerOne = new Player("human", "./assets/human.png")
         this.playerTwo = new Player("computer", "./assets/robot.png")
-        this.gameMode = gameMode
+        this.gameMode
         this.winStates = {
             rock: ['scissors', 'lizard'],
             paper: ['rock', 'alien'],
