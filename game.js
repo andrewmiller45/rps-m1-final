2512b9a2fa80eae5c627f23bba03a0804f937838
Andrew Miller <andrew.r.miller45@gmail.com>
2022-06-09 14:24:42 -0700

    completed dom manipulation for functioning site

--- game.js
+++ game.js
@@ -5,6 +5,7 @@ class Game {
         this.playerTwo = new Player("computer", "./assets/robot.png")
         this.gameMode 
         this.playerOneChoice 
+        this.winMsg
         this.winStates = {
             rock: ['scissors', 'lizard'],
             paper: ['rock', 'alien'],
@@ -17,13 +18,16 @@ class Game {
             var cpuChoice = this.playerTwo.takeTurn(gameMode)
             if (this.winStates[cpuChoice].includes(this.playerOneChoice)) {
                 this.playerTwo.increaseWins()
+                this.winMsg = `Computer Wins!`
                 return console.log(cpuChoice, 'computer wins')
             }
             else if(this.winStates[playerOneChoice].includes(cpuChoice)){
                 this.playerOne.increaseWins()
+                this.winMsg = `Nice!  You win!`
                 return console.log(cpuChoice, 'player wins')
             }
             else 
+                this.winMsg = `It's a draw!`
                 return console.log(cpuChoice, 'draw')
             }
         }
--- main.js
+++ main.js
@@ -6,34 +6,85 @@ var changeGameButton = document.getElementById('changeGame')
 var humanWins = document.getElementById('humanWins')
 var cpuWins = document.getElementById('cpuWins')
 var gameChoiceContainer = document.querySelector('.choices-styling')
-
+var fighterIconsView = document.querySelector('.choices-container')
+var resultView = document.getElementById('resultMsg')
+var choiceHeader = document.getElementById('gameChoicePrompt')
 
 gameChoiceContainer.addEventListener('click', selectGamemode)
+changeGameButton.addEventListener('click', returnToGameSelect)
+fighterIconsView.addEventListener('click', chooseFighter)
 
 function hide(element){
-    element.classList.add('.hidden')
+    element.classList.add('hidden')
 }
 
 function show(element){
-    element.classList.remove('.hidden')
+    element.classList.remove('hidden')
 }
 
 function selectGamemode (e){
     if(e.target.parentElement.id === 'spicy'){
-        console.log('test');
+        hide(gameChoiceContainer)
+        show(changeGameButton)
+        hide(choiceHeader)
+        newGame.gameMode = 'spicy'
+        fighterIconsView.innerHTML = 
+        `<article class="choices-container">
+          <img class="fighter" id="alien" src="./assets/lines-alien.png" alt="alien image">
+          <img class="fighter" id="paper" src="./assets/lines-paper.png" alt="paper image">
+          <img class="fighter" id="scissors"  src="./assets/lines-scissors.png" alt="scissors image">
+          <img class="fighter" id="lizard"  src="./assets/lizard.png" alt="lizard image">
+          <img class="fighter" id="rock"  src="./assets/rock.png" alt="rock image">
+        </article>`
+    }
+
+    if(e.target.parentElement.id === 'vanilla'){
+        hide(gameChoiceContainer)
+        show(changeGameButton)
+        hide(choiceHeader)
+        newGame.gameMode = 'vanilla'
+        fighterIconsView.innerHTML = 
+            `<article class="choices-container">
+            <img class="fighter" id="paper"  src="./assets/lines-paper.png" alt="paper image">
+            <img class="fighter" id="scissors"  src="./assets/lines-scissors.png" alt="scissors image">
+            <img class="fighter" id="rock"  src="./assets/rock.png" alt="rock image">
+            </article>`
     }
 }
 
+function returnToGameSelect(){
+    show(gameChoiceContainer)
+    hide(changeGameButton)
+    show(choiceHeader)
+    fighterIconsView.innerHTML = "" 
+}
+
 function chooseFighter(e){
-    //newGame.playerOneChoice = e.target.id
+    newGame.playerOneChoice = e.target.id
+    runGame()
+    updateWinsView()
+    hide(fighterIconsView)
+    setTimeout(resetGame, 2000)
 }
 
 function resetGame(){
     newGame.playerOneChoice = ""
     newGame.playerTwoChoice = ""
-
+    hide(resultView)
+    show(fighterIconsView)
 }
 
-function changeGameType(){
+function runGame(){
+    newGame.checkWinStates(newGame.playerOneChoice, newGame.gameMode)
+    displayResult()
+}
 
+function updateWinsView(){
+    humanWins.innerText = `Wins: ${newGame.playerOne.wins}`
+    cpuWins.innerText = `Wins: ${newGame.playerTwo.wins}`
+}
+
+function displayResult(){
+    resultView.innerText = `${newGame.winMsg}`
+    show(resultView)
 }
