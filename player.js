ae10dc34c65be187554bf6a7f5845dfc6fb55cbd
Andrew Miller <andrew.r.miller45@gmail.com>
2022-06-10 19:50:10 -0700

    remove semi colons

--- player.js
+++ player.js
@@ -8,11 +8,11 @@ class Player {
     takeTurn(gameMode){
         if (gameMode === 'vanilla') {
             var fighters = ['rock', 'paper', 'scissors']
-            return fighters[Math.floor(Math.random() * fighters.length)]; 
+            return fighters[Math.floor(Math.random() * fighters.length)]
         }
         if (gameMode === 'spicy') {
             var fighters = ['rock', 'paper', 'scissors', 'lizard', 'alien']
-            return fighters[Math.floor(Math.random() * fighters.length)];
+            return fighters[Math.floor(Math.random() * fighters.length)]
         }
     }
 
